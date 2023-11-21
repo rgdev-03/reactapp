6780abd311f1e47196d14826685eee488a95fc48
grp-03 <grpdev.03@gmail.com>
2023-11-21 20:06:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Dashboard  from './components/dashboard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={ Home} />
+                    <Route path="/login" component={ Login } />
+                    <Route path="/signup" component={ Signup } />
+                    <Route path="/Home" component={ Dashboard } />
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
